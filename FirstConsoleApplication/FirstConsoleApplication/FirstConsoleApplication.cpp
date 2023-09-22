0221b56f8843eefadf52399af8a2a45f96f6f702
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-22 12:31:36 +0200

    elzorlo

--- FirstConsoleApplication/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ FirstConsoleApplication/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -1,30 +1,33 @@
-// FirstConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#define _USE_MATH_DEFINES
 #include <iostream>
+using namespace std;
 
+
+/*
+Podstawy programowania:
+
+1.
+2. Program obliczaj¹cy pole prostok¹ta.
+3. Program obliczaj¹cy objêtoœæ sto¿ka.
+4. Program obliczaj¹cy pole ko³a.
+5. Program obliczaj¹cy wartoœæ wyra¿enia a^2 + b^2
+6. Program obliczaj¹cy pole trójk¹ta o podstawie b i wysokoœci h
+7. Program obliczaj¹cy objêtoœæ kuli o promieniu r
+8. Program obliczaj¹cy pole trapezu o podstawach a i b oraz wysokoœci h
+9. Program obliczaj¹cy œredni¹ wa¿on¹ trzech liczb z podanymi odpowiednimi wagami w1, w2 i w3.
+*/
+
+//Program obliczaj¹cy œredni¹ arytmetyczn¹ dwóch liczb.
+    
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << -59.5 << 'x' << "asdfasdfhasdfasdfhvasdfjh" << true << false << elo zelo"\n";
-
-    int number;
-    number = 5;
-    
-    int secondNumber;
-    secondNumber = number + 2;
-    x = 3 << 1; //3 / (2^1) = 6 011 110
-
-    /*
-    Piorytety operatorów:
-
-    1.(,)
-    2.*,/,%
-    3.+,-
-    4.=
-
-    */
-
+    double a;
+    double b;
+    cout << "Type the first number: " << endl;
+    cin >> a;
+    cout << "Type the second number: " << endl;
+    cin >> b;
+    cout << a + b;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -36,4 +39,47 @@ int main()
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file                                                                       
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file 
+void test() {
+cout << "Hello World!\n";
+cout << -59.5 << 'x' << "asdfasdfhasdfasdfhvasdfjh" << true << false << "elozelo\n";
+
+int number;
+number = 5;
+
+int secondNumber;
+secondNumber = number + 2;
+int x;
+x = 3 << 1; //3 / (2^1) = 6 011 110
+
+/*
+Piorytety operatorów:
+
+1.(,)
+2.*,/,%
+3.+,-
+4.=
+
+*/
+cout << "Hello World!\n";
+cout << -59.5 << 'x' << "ghfghjfghj" << true << false << '\n';
+
+//typ nazwa;
+int number;
+number = 5;
+
+int secondNumber;
+secondNumber = number + 2;
+/*
+Priorytet operatorów:
+
+(, )
++, - (unarne)
+*, /, %
++, -
+=
+
+*/
+std::cout << "Komuinikat \n";
+std::cin >> secondNumber >> number;
+}
