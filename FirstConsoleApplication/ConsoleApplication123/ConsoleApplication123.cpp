aace75ce31b3a170da5af112ba3a87f18bc4b892
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-29 12:27:35 +0200

    elzoelo

---
+++ FirstConsoleApplication/ConsoleApplication123/ConsoleApplication123.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void task1(){
+
+    double a;
+    double b;
+    cout << "Podaj pierwsz¹ liczbe: " << endl;
+    cin >> a;
+    cout << "Podaj drug¹ liczbe: " << endl;
+    cin >> b;
+    cout << "Twoja liczba to: " << a / b << endl;
+}
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    task1();
+}
